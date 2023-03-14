e954c55cc8adb8f92c7d106e449343c395a6740a
mAkeddar <mehdi.akeddar@gmail.com>
2023-03-14 21:01:03 +0100

    ant design instead of material ui

--- portato/src/Components/BottomBar.tsx
+++ portato/src/Components/BottomBar.tsx
@@ -1,44 +1,39 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import HomeIcon from '@mui/icons-material/Home';
-import ChatIcon from '@mui/icons-material/Chat';
-import LocalShippingIcon from '@mui/icons-material/LocalShipping';
-import PersonIcon from '@mui/icons-material/Person';
+import React, { useState } from 'react';
+import { Layout, Menu } from 'antd';
+import {
+  HomeOutlined,
+  ProfileOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from '@ant-design/icons';
 
-const BottomBarNavigator: React.FC = () => {
-  const [value, setValue] = React.useState(0);
-  const navigate = useNavigate();
+const { Footer } = Layout;
 
-  const handleClick = (newValue: number) => {
-    setValue(newValue);
-    switch (newValue) {
-      case 0:
-        navigate('/');
-        break;
-      case 1:
-        navigate('/chat');
-        break;
-      case 2:
-        navigate('/about');
-        break;
-      case 3:
-        navigate('/profile');
-        break;
-      default:
-        break;
-    }
+const BottomNavigation: React.FC = () => {
+  const [current, setCurrent] = useState<string>('home');
+
+  const handleClick = (e: any) => {
+    setCurrent(e.key);
   };
 
   return (
-    <BottomNavigation value={value} onChange={(event, newValue) => handleClick(newValue)} color = "secondary">
-      <BottomNavigationAction label="Home" icon={<HomeIcon />} />
-      <BottomNavigationAction label="Chat" icon={<ChatIcon />} />
-      <BottomNavigationAction label="Shippements" icon={<LocalShippingIcon />} />
-      <BottomNavigationAction label="Profile" icon={<PersonIcon />} />
-    </BottomNavigation>
+    <Footer>
+      <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
+        <Menu.Item key="home" icon={<HomeOutlined />}>
+          Home
+        </Menu.Item>
+        <Menu.Item key="profile" icon={<ProfileOutlined />}>
+          Profile
+        </Menu.Item>
+        <Menu.Item key="mail" icon={<MailOutlined />}>
+          Mail
+        </Menu.Item>
+        <Menu.Item key="settings" icon={<SettingOutlined />}>
+          Settings
+        </Menu.Item>
+      </Menu>
+    </Footer>
   );
 };
 
-export default BottomBarNavigator;
+export default BottomNavigation;
--- portato/src/Components/Navbar.tsx
+++ portato/src/Components/Navbar.tsx
@@ -1,89 +1,17 @@
-import React, { FC, ReactElement } from "react";
-import {
-  Box,
-  Link,
-  Container,
-  IconButton,
-  Menu,
-  MenuItem,
-  Toolbar,
-  Typography,
-} from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import CustomIcon from "../Images/favicon_v001.png"
-import { routes } from "../routes";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { Layout, Menu } from 'antd';
+import { HomeOutlined } from '@ant-design/icons';
 
-const Navbar: FC = (): ReactElement => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
+const { Header } = Layout;
 
-  const handleOpenNavMenu = (event: any) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
+const HeaderBar: React.FC = () => {
   return (
-    <Box
-      sx={{
-        width: "100%",
-        height: "auto",
-        backgroundColor: "secondary.main",
-      }}
-    >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-            }}
-          >
-            Portato icon
-          </Typography>
-          <Typography
-            variant="h5"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" }, justifyContent:"center", textAlign : "center" }}
-            
-          >
-            Portato
-          </Typography>
-          
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            <Box
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "flex-start",
-                alignItems: "center",
-                marginLeft: "1rem",
-              }}
-            >
-              {routes.map((page) => (
-                <Link
-                  key={page.key}
-                  component={NavLink}
-                  to={page.path}
-                  color="black"
-                  underline="none"
-                  variant="button"
-                  sx={{ fontSize: "large", marginLeft: "2rem" }}
-                >
-                  {page.title}
-                </Link>
-              ))}
-            </Box>
-          </Box>
-        </Toolbar>
-      </Container>
-    </Box>
+    <Header>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+        <img src="logo.png" alt="Logo" style={{ height: '50px' }} />
+      </div>
+    </Header>
   );
 };
 
-export default Navbar;
+export default HeaderBar;
