a76ef348dd761d0c37b9f215fbe46e0975d2d722
hugo2410 <hugobirch@hotmail.fr>
2023-03-31 11:02:33 +0200

    updates package rules and makes pwa downloadable

--- portato/src/index.tsx
+++ portato/src/index.tsx
@@ -4,6 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBgwwBX5CawlagVbS4FN99GNAAxN9u_SIU",
+  authDomain: "portatoapp.firebaseapp.com",
+  databaseURL: "https://portatoapp-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "portatoapp",
+  storageBucket: "portatoapp.appspot.com",
+  messagingSenderId: "22027928847",
+  appId: "1:22027928847:web:c98f61498ea7101d6f2a7a",
+  measurementId: "G-0XSWEVBGXP"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
