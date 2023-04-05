67d3206ea3fe7823bcc9831b483ab6703e1e30f2
hugo2410 <hugobirch@hotmail.fr>
2023-04-05 09:42:02 +0200

    wip

--- portato/src/Pages/Home.tsx
+++ portato/src/Pages/Home.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Typography, Button } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import PageLayout from "./PageLayout"
+import axios from 'axios';
 
 const { Title } = Typography;
 
 const Home: React.FC = () => {
+
+  const [routeData, setRouteData] = useState(null);
   const navigate = useNavigate();
 
   const handleSendClick = () => {
+      //fetchRoute();
     navigate('/send');
   };
 
   const handleDeliverClick = () => {
     navigate('/deliver');
+  };
+
+
+  const fetchRoute = async () => {
+    const apiKey = process.env.REACT_APP_OPENROUTESERVICE_API_KEY;
+
+    const coordinates = '8.681495,49.41461|8.686507,49.41943'; // Replace with your desired coordinates
+    const profile = 'driving-car'; // Change the profile according to your needs (e.g., 'foot-walking', 'cycling-regular', etc.)
+
+    const url = `https://api.openrouteservice.org/v2/directions/${profile}?api_key=${apiKey}&coordinates=${coordinates}`;
+
+    try {
+      const response = await axios.get(url);
+      setRouteData(response.data);
+      console.log(response.data, routeData);
+    } catch (error) {
+      console.error('Error fetching route data:', error);
+    }
   };
 
   return (
