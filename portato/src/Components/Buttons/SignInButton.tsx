686208d8c17a380e559bb944abfc45d0a2d996ed
hugo2410 <hugobirch@hotmail.fr>
2023-04-20 16:31:26 +0200

    Adds login if not signed at summary page and adds firebaseui comp

---
+++ portato/src/Components/Buttons/SignInButton.tsx
@@ -0,0 +1,17 @@
+// SignInButton.tsx
+import React from 'react';
+import { Button } from 'antd';
+
+interface SignInButtonProps {
+  onClick: () => void;
+}
+
+const SignInButton: React.FC<SignInButtonProps> = ({ onClick }) => {
+  return (
+    <Button type="primary" onClick={onClick} style={{ margin: '16px' }}>
+      Sign In
+    </Button>
+  );
+};
+
+export default SignInButton;
--- portato/src/Pages/Sender/Summary.tsx
+++ portato/src/Pages/Sender/Summary.tsx
@@ -1,28 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PageLayout from "../Layouts/PageLayoutTest"
-import { Typography, Card, ConfigProvider} from 'antd';
+import { Typography, Card } from 'antd';
 import ProgressBar from "../../Components/ProgressBar";
 import ConfirmButtom from "../../Components/Buttons/ConfirmButton";
 import BackButton from "../../Components/Buttons/BackButton";
+import SignInButton from "../../Components/Buttons/SignInButton"; // Import the SignInButton component
 
 import { useSelector } from "react-redux";
-import {ObjectInfoState, IObjectInfo} from '../../type'
+import { IObjectInfo } from '../../type';
 import { fetchDataOnce } from "../../linksStoreToFirebase";
+import { auth } from "../../firebaseConfig"; // Import the auth instance
+import { onAuthStateChanged, User } from "firebase/auth"; // Import the onAuthStateChanged function
+
 
 const { Title } = Typography;
-const progress = 100
+const progress = 100;
 
 const Summary: React.FC = () => {
-    const nextScreen = "/"
-    const objecInfo = useSelector((state: IObjectInfo) => state);
-    console.log(objecInfo)
-    // Fetch data once
-    fetchDataOnce();
+  const nextScreen = "/";
+  const objecInfo = useSelector((state: IObjectInfo) => state);
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  console.log(objecInfo);
+  fetchDataOnce();
+
 
     return (
         <PageLayout>
-            <ProgressBar progress = {progress}/>
-              <Card bordered={true} style={{marginLeft:'10%', width:'80%', marginTop:'20%',backgroundColor:"#FFF4E4"}}>
+            <ProgressBar progress={progress} />
+            <Card
+                bordered={true}
+                style={{ marginLeft: "10%", width: "80%", marginTop: "20%", backgroundColor: "#FFF4E4" }}>
                 <Title level = {2}> Summary </Title>
                         <div >
                             <Title level = {4}>  {objecInfo.name}</Title>
@@ -44,10 +66,16 @@ const Summary: React.FC = () => {
                             <Title level = {4}> Price</Title>
                             <Typography >  {objecInfo.price} CHF </Typography>
                         </div>
-            </Card>
-            <ConfirmButtom/>
-            <BackButton/>
-        </PageLayout>
+                        </Card>
+      {user ? (
+        <ConfirmButtom />
+      ) : (
+        <SignInButton onClick={() => {
+          // Redirect to the sign-in page or open a sign-in modal
+        }} />
+      )}
+      <BackButton />
+    </PageLayout>
   );
 };
 
