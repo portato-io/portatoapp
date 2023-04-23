40108735878267e4d52f67af9200101ba112223d
hugo2410 <hugobirch@hotmail.fr>
2023-04-23 19:43:03 +0200

    fixes firebaseauth component

--- portato/src/Components/FirebaseAuth.tsx
+++ portato/src/Components/FirebaseAuth.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom"; // Import useNavigate
+import { useNavigate } from "react-router-dom";
 import "firebaseui/dist/firebaseui.css";
 import { auth } from "../firebaseConfig";
 import { GoogleAuthProvider, EmailAuthProvider } from "firebase/auth";
@@ -7,27 +7,20 @@ import * as firebaseui from "firebaseui";
 
 const FirebaseAuth = () => {
   const uiRef = useRef<HTMLDivElement | null>(null);
-  const navigate = useNavigate(); // Get the useNavigate instance
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // Check if the FirebaseUI widget is already rendered
-    if (uiRef.current) {
-      return;
-    }
-
     // Initialize FirebaseUI
     const uiConfig = {
-      signInSuccessUrl: "/enterObjInfo", // e.g., "index.html"
+      signInSuccessUrl: "/enterObjInfo",
       signInOptions: [
-        // List the authentication providers you want to support
         GoogleAuthProvider.PROVIDER_ID,
         EmailAuthProvider.PROVIDER_ID,
       ],
       callbacks: {
         signInSuccessWithAuthResult: () => {
-          // Redirect the user to the previous page after successful sign-in
-          navigate(-1); // Navigate back to the previous page
-          return false; // Prevent default redirection by FirebaseUI
+          navigate(-1);
+          return false;
         },
       },
     };
@@ -35,9 +28,7 @@ const FirebaseAuth = () => {
     // Initialize the FirebaseUI widget
     const ui = new firebaseui.auth.AuthUI(auth);
     ui.start("#firebaseui-auth-container", uiConfig);
-  }, [navigate]); // Add navigate as a dependency
-
-  console.log(uiRef)
+  }, [navigate]);
 
   return <div id="firebaseui-auth-container" ref={uiRef}></div>;
 };
