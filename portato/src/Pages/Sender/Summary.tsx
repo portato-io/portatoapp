fc3554992a055c65c69af7d2967bd79a67ab9076
hugo2410 <hugobirch@hotmail.fr>
2023-04-26 19:36:14 +0200

    Update portato/src/Pages/Sender/Summary.tsx

--- portato/src/Pages/Sender/Summary.tsx
+++ portato/src/Pages/Sender/Summary.tsx
@@ -34,7 +34,6 @@ const Summary: React.FC = () => {
     setIsModalVisible(false);
   };
   const objecInfo = useSelector((state: IObjectInfo) => state);
-  console.log(objecInfo);
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
