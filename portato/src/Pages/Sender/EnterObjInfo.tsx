f3d3e430a79338a93c5879d80c7d8a44a3d9cee5
mAkeddar <mehdi.akeddar@gmail.com>
2023-04-29 23:49:31 +0200

    first fix of form size and scrren/ added a scrolling optiin the form

--- portato/src/Pages/Sender/EnterObjInfo.tsx
+++ portato/src/Pages/Sender/EnterObjInfo.tsx
@@ -22,12 +22,9 @@ import {
   uploadBytesResumable,
   getDownloadURL,
   StorageError,
-  UploadTaskSnapshot,
 } from "firebase/storage";
 import { storage } from "../../firebaseConfig";
-//TMP
 
-import { Button } from "antd";
 import {
   setObject,
   addObjectImages,
@@ -135,11 +132,6 @@ const EnterObjInfo: React.FC = () => {
 
   const dispatch = useDispatch();
 
-  const submitForm = (e: any) => {
-    e.preventDefault();
-    // dispatch FORM_SUBMIT action
-  };
-
   const handleInputChange = (e: any) => {
     setValues({
       ...object,
@@ -163,151 +155,156 @@ const EnterObjInfo: React.FC = () => {
     });
   };
 
+  const containerHeight = window.innerHeight * 0.7;
+  console.log(containerHeight + "px");
   return (
     <PageLayout>
       <ProgressBar />
-      <Form
-        id="sender-forms"
-        onFinish={onFinish}
-        className="form-sender"
-        labelCol={{ span: 4 }}
-        wrapperCol={{ span: 14 }}
-        layout="horizontal"
+      <Title
+        level={3}
+        style={{ marginLeft: "5%", marginTop: "15%", background: "#fff" }}
       >
-        <Title level={3} style={{ background: "#fff" }}>
-          {" "}
-          What would you like to ship?
-        </Title>
-        <Form.Item
-          label={<label className="item-form-label">Name</label>}
-          //name="name"
-          //rules={[{ required: true, message: 'Please input your username!' }]}
+        {" "}
+        What would you like to ship?
+      </Title>
+      <div style={{ height: containerHeight + "px", overflowY: "scroll" }}>
+        <Form
+          id="sender-forms"
+          onFinish={onFinish}
+          className="form-sender"
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 14 }}
+          layout="horizontal"
         >
-          <Input
-            name="name"
-            value={object.name}
-            onChange={handleInputChange}
-            placeholder="The title of your shipment"
-            style={{ width: "90%" }}
-          />
-        </Form.Item>
-
-        <Form.Item
-          label={<label className="item-form-label">Description</label>}
-
-          //rules={[{ required: true, message: 'Please input description!' }]}
-        >
-          <TextArea
-            name="description"
-            value={object.description}
-            onChange={handleInputChange}
-            rows={3}
-            placeholder="eg: It’s a good idea to specify the dimensions of large items."
-            style={{ width: "90%" }}
-          />
-        </Form.Item>
-
-        <Form.Item label={<label className="item-form-label">Size</label>}>
-          <Radio.Group
-            name="size"
-            value={object.size}
-            onChange={handleInputChange}
-            style={{ marginLeft: "18%" }}
+          <Form.Item
+            label={<label className="item-form-label">Name</label>}
+            //name="name"
+            //rules={[{ required: true, message: 'Please input your username!' }]}
           >
-            <Radio value={"S"}>S</Radio>
-            <Radio value={"M"}>M</Radio>
-            <Radio value={"L"}>L</Radio>
-            <Radio value={"XL"}>XL</Radio>
-          </Radio.Group>
-        </Form.Item>
+            <Input
+              name="name"
+              value={object.name}
+              onChange={handleInputChange}
+              placeholder="The title of your shipment"
+              style={{ width: "90%" }}
+            />
+          </Form.Item>
 
-        <Form.Item label={<label className="item-form-label">Weight</label>}>
-          <Radio.Group
-            name="weight"
-            value={object.weight}
-            onChange={handleInputChange}
-            style={{ marginLeft: "15%" }}
+          <Form.Item
+            label={<label className="item-form-label">Description</label>}
+            //rules={[{ required: true, message: 'Please input description!' }]}
           >
-            <Radio.Button value="-5 kg" style={{ background: "#F8F9FE" }}>
-              -5 kg
-            </Radio.Button>
-            <Radio.Button value="5-20 kg" style={{ background: "#F8F9FE" }}>
-              5-20 kg
-            </Radio.Button>
-            <Radio.Button value="+20 kg" style={{ background: "#F8F9FE" }}>
-              +20 kg
-            </Radio.Button>
-          </Radio.Group>
-        </Form.Item>
+            <TextArea
+              name="description"
+              value={object.description}
+              onChange={handleInputChange}
+              rows={3}
+              placeholder="eg: It’s a good idea to specify the dimensions of large items."
+              style={{ width: "90%" }}
+            />
+          </Form.Item>
 
-        <Form.Item
-          label={<label className="item-form-label">Upload images</label>}
-          valuePropName="fileList"
-        >
-          <Upload
-            action="/upload.do"
-            listType="picture-card"
-            fileList={fileList}
-            beforeUpload={beforeUpload}
-            onRemove={handleRemove}
-            itemRender={(originNode, file, fileList) => {
-              if (file.status === "uploading" || file.status === "done") {
-                return (
-                  <div style={{ position: "relative" }}>
-                    <img
-                      src={
-                        file.url ||
-                        (file.originFileObj
-                          ? URL.createObjectURL(file.originFileObj)
-                          : "")
-                      }
-                      alt={file.name}
-                      style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                        borderRadius: "4px",
-                      }}
-                    />
-                    {file.status === "uploading" && (
-                      <Progress
-                        type="circle"
-                        percent={file.percent}
-                        width={40}
+          <Form.Item label={<label className="item-form-label">Size</label>}>
+            <Radio.Group
+              name="size"
+              value={object.size}
+              onChange={handleInputChange}
+              style={{ marginLeft: "18%" }}
+            >
+              <Radio value={"S"}>S</Radio>
+              <Radio value={"M"}>M</Radio>
+              <Radio value={"L"}>L</Radio>
+              <Radio value={"XL"}>XL</Radio>
+            </Radio.Group>
+          </Form.Item>
+
+          <Form.Item label={<label className="item-form-label">Weight</label>}>
+            <Radio.Group
+              name="weight"
+              value={object.weight}
+              onChange={handleInputChange}
+              style={{ marginLeft: "15%" }}
+            >
+              <Radio.Button value="-5 kg" style={{ background: "#F8F9FE" }}>
+                -5 kg
+              </Radio.Button>
+              <Radio.Button value="5-20 kg" style={{ background: "#F8F9FE" }}>
+                5-20 kg
+              </Radio.Button>
+              <Radio.Button value="+20 kg" style={{ background: "#F8F9FE" }}>
+                +20 kg
+              </Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+
+          <Form.Item
+            label={<label className="item-form-label">Upload images</label>}
+            valuePropName="fileList"
+          >
+            <Upload
+              action="/upload.do"
+              listType="picture-card"
+              fileList={fileList}
+              beforeUpload={beforeUpload}
+              onRemove={handleRemove}
+              itemRender={(originNode, file, fileList) => {
+                if (file.status === "uploading" || file.status === "done") {
+                  return (
+                    <div style={{ position: "relative" }}>
+                      <img
+                        src={
+                          file.url ||
+                          (file.originFileObj
+                            ? URL.createObjectURL(file.originFileObj)
+                            : "")
+                        }
+                        alt={file.name}
                         style={{
-                          position: "absolute",
-                          top: "50%",
-                          left: "50%",
-                          transform: "translate(-50%, -50%)",
+                          width: "100%",
+                          height: "100%",
+                          objectFit: "cover",
+                          borderRadius: "4px",
                         }}
                       />
-                    )}
-                    <div
-                      style={{
-                        position: "absolute",
-                        top: "2px",
-                        right: "2px",
-                        background: "rgba(255, 255, 255, 0.8)",
-                        borderRadius: "4px",
-                        cursor: "pointer",
-                      }}
-                      onClick={() => handleRemove(file)}
-                    >
-                      <CloseOutlined />
+                      {file.status === "uploading" && (
+                        <Progress
+                          type="circle"
+                          percent={file.percent}
+                          width={40}
+                          style={{
+                            position: "absolute",
+                            top: "50%",
+                            left: "50%",
+                            transform: "translate(-50%, -50%)",
+                          }}
+                        />
+                      )}
+                      <div
+                        style={{
+                          position: "absolute",
+                          top: "2px",
+                          right: "2px",
+                          background: "rgba(255, 255, 255, 0.8)",
+                          borderRadius: "4px",
+                          cursor: "pointer",
+                        }}
+                        onClick={() => handleRemove(file)}
+                      >
+                        <CloseOutlined />
+                      </div>
                     </div>
-                  </div>
-                );
-              }
-              return originNode;
-            }}
-          >
-            {fileList.length >= 8 ? null : uploadButton}
-          </Upload>
-        </Form.Item>
-
-        <NextButton nextScreen={nextScreen} />
-        <BackButton />
-      </Form>
+                  );
+                }
+                return originNode;
+              }}
+            >
+              {fileList.length >= 8 ? null : uploadButton}
+            </Upload>
+          </Form.Item>
+          <NextButton nextScreen={nextScreen} />
+          <BackButton />
+        </Form>
+      </div>
     </PageLayout>
   );
 };
