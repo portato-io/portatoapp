b37a75b044f9fcce52da81395deea2516659e822
hugo2410 <hugobirch@hotmail.fr>
2023-05-03 14:09:01 +0200

    test

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -4,5 +4,5 @@ import AuthWrapper from '../Components/AuthWrapper';
 import ProfileContent from '../Components/ProfileContent';
 
 const Profile: React.FC = () => <AuthWrapper Component={ProfileContent} />;
-
+console.log("Profile");
 export default Profile;
