46f22be3aaa218a9f8ed93576896636b8378682a
hugo2410 <hugobirch@hotmail.fr>
2023-05-08 12:20:30 +0200

    add routes

---
+++ src/Pages/Admin/AllRoutes.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import UserRequests from '../userRequests'; // Import UserRequests from the correct path
+import PageLayout from '../Layouts/PageLayoutTest';
+
+const UserRequestsScreen: React.FC = () => {
+  return (
+    <PageLayout>
+      <div>
+        <UserRequests />
+      </div>
+    </PageLayout>
+  );
+};
+
+export default UserRequestsScreen;
