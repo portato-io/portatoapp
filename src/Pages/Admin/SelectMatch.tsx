62ce9e42bd0f67b908b063421891e79a8293aedb
hugo2410 <hugobirch@hotmail.fr>
2023-05-18 18:26:03 +0200

    wip

--- src/Pages/Admin/SelectMatch.tsx
+++ src/Pages/Admin/SelectMatch.tsx
@@ -1,40 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import FetchRoutes from '../../Components/FetchRoutes';
+import React, { useState } from 'react';
 import PageLayout from '../Layouts/PageLayoutTest';
 import { database } from '../../firebaseConfig';
 import { ref, get } from 'firebase/database';
 
-const ADMIN = true;
+const SelectMatch: React.FC<{
+  route_id: string;
+}> = () => {
+  const [inputValue, setInputValue] = useState('');
 
-const SelectMatch: React.FC = () => {
-  const [userIds, setUserIds] = useState<string[]>([]);
-
-  const fetchUserIds = async (): Promise<void> => {
-    const fetchedUserIds: string[] = [];
-    const usersRef = ref(database, 'users');
-    const snapshot = await get(usersRef);
-
-    snapshot.forEach((childSnapshot) => {
-      const userId = childSnapshot.key;
-      if (userId) {
-        fetchedUserIds.push(userId);
-      }
-    });
-
-    setUserIds(fetchedUserIds);
+  const suggestRequest = (value: string) => {
+    // implement your function here
+    console.log('Value submitted: ', value);
   };
 
-  useEffect(() => {
-    fetchUserIds();
-  }, []);
-  const containerHeight = window.innerHeight * 0.8;
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    suggestRequest(inputValue);
+  };
+
   return (
     <PageLayout>
-      <div style={{ height: containerHeight + 'px', overflowY: 'scroll' }}>
-        {userIds.map((uid) => (
-          <FetchRoutes key={uid} uid={uid} admin={ADMIN} />
-        ))}
-      </div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter your text here"
+      />
+      <button onClick={handleSubmit}>Validate</button>
     </PageLayout>
   );
 };
