8562986b6bf1a097e469bad6b3523fc2f9f0b7e7
mAkeddar <mehdi.akeddar@gmail.com>
2023-06-06 13:24:09 +0200

    first firebase profile picture

--- src/Components/PageComponents/ProfileContent.tsx
+++ src/Components/PageComponents/ProfileContent.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PageLayout from '../../Pages/Layouts/PageLayoutTest';
 import { List, Card } from 'antd-mobile';
-import { UserOutlined } from '@ant-design/icons';
+import { UserOutlined, PlusOutlined } from '@ant-design/icons';
 import { auth } from '../../firebaseConfig';
 import { signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { checkAdmin } from '../AuthProvider';
+import ProfilePictureUpload from '../ProfilePictureUpload';
 
 function ProfileContent() {
   const navigate = useNavigate();
@@ -36,11 +37,21 @@ function ProfileContent() {
   const handleSupportClick = () => {
     navigate('/contact_support');
   };
+  const handleProfilePictureClick = () => {
+    // Logic to handle the click event for profile picture
+    // You can open a modal or perform any other action here
+    console.log('Profile picture clicked');
+  };
+
+  const handleAddPictureClick = () => {
+    // Logic to handle the click event for adding a picture
+    // You can open a file picker or camera interface here
+    console.log('Add picture clicked');
+  };
 
   return (
     <PageLayout display={display}>
       <div style={{ display: display }} className="profile_content">
-        {/* Rest of your code... */}
         <div
           style={{
             height: '100vh',
@@ -58,59 +69,71 @@ function ProfileContent() {
               justifyContent: 'center',
             }}
           >
-            <div
-              style={{
-                width: 'min(min(50vw,50vh),250px)',
-                height: 'min(min(50vw,50vh),250px)',
-                borderRadius: '50%',
-                backgroundColor: '#fff',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <UserOutlined style={{ fontSize: '48px' }} />
-            </div>
+            {/* <div
+            //   style={{
+            //     width: 'min(min(50vw,50vh),250px)',
+            //     height: 'min(min(50vw,50vh),250px)',
+            //     borderRadius: '50%',
+            //     backgroundColor: '#fff',
+            //     display: 'flex',
+            //     alignItems: 'center',
+            //     justifyContent: 'center',
+            //   }}
+            //   onClick={handleProfilePictureClick}
+            // >
+            //   <UserOutlined style={{ fontSize: '48px' }} />
+            //   <PlusOutlined style={{ fontSize: '24px' }} />
+              
+            </div> */}
+            <ProfilePictureUpload />
           </div>
           <div
             style={{
-              backgroundColor: '#2897FF',
-              flex: 6,
-              height: '80vh',
-              overflowY: 'auto',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
             }}
           >
-            <Card style={{ borderRadius: '5%', height: '100%' }}>
-              <List mode="card" style={{ marginTop: '1vh' }}>
-                <List.Item arrow={true} onClick={handleMyAccountClick}>
-                  My Account
-                </List.Item>
-                <List.Item arrow={true} onClick={handleMyPaymentMethodsClick}>
-                  My Payment Methods
-                </List.Item>
-                <List.Item arrow={true} onClick={handleMyDeliveriesClick}>
-                  My Deliveries
-                </List.Item>
-                <List.Item arrow={true} onClick={handleMySendRequestsClick}>
-                  My Send Requests
-                </List.Item>
-                <List.Item arrow={true} onClick={handleSupportClick}>
-                  Support
-                </List.Item>
-                {isAdmin && (
-                  <List.Item arrow={true} onClick={handleAdminClick}>
-                    Admin window
+            <div
+              style={{
+                backgroundColor: '#2897FF',
+                flex: 6,
+                height: '80vh',
+                overflowY: 'auto',
+              }}
+            >
+              <Card style={{ borderRadius: '5%', height: '100%' }}>
+                <List mode="card" style={{ marginTop: '1vh' }}>
+                  <List.Item arrow={true} onClick={handleMyAccountClick}>
+                    My Account
                   </List.Item>
-                )}
-                <List.Item
-                  arrow={true}
-                  onClick={() => signOut(auth)}
-                  style={{ color: 'red' }}
-                >
-                  Sign out
-                </List.Item>
-              </List>
-            </Card>
+                  <List.Item arrow={true} onClick={handleMyPaymentMethodsClick}>
+                    My Payment Methods
+                  </List.Item>
+                  <List.Item arrow={true} onClick={handleMyDeliveriesClick}>
+                    My Deliveries
+                  </List.Item>
+                  <List.Item arrow={true} onClick={handleMySendRequestsClick}>
+                    My Send Requests
+                  </List.Item>
+                  <List.Item arrow={true} onClick={handleSupportClick}>
+                    Support
+                  </List.Item>
+                  {isAdmin && (
+                    <List.Item arrow={true} onClick={handleAdminClick}>
+                      Admin window
+                    </List.Item>
+                  )}
+                  <List.Item
+                    arrow={true}
+                    onClick={() => signOut(auth)}
+                    style={{ color: 'red' }}
+                  >
+                    Sign out
+                  </List.Item>
+                </List>
+              </Card>
+            </div>
           </div>
         </div>
       </div>
---
+++ src/Components/ProfilePictureUpload.tsx
@@ -0,0 +1,63 @@
+import React, { ChangeEvent, useState } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC3X0ZVQIbTvJIpJfDGdrc2q8_JlSVzDT4',
+  authDomain: 'portatoapp.firebaseapp.com',
+  databaseURL:
+    'https://portatoapp-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'portatoapp',
+  storageBucket: 'portatoapp.appspot.com',
+  messagingSenderId: '22027928847',
+  appId: '1:22027928847:web:3588209ce56727486f2a7a',
+  measurementId: 'G-Y4VZJ8CMPY',
+};
+
+firebase.initializeApp(firebaseConfig);
+const ProfilePictureUpload: React.FC = () => {
+  const [profilePicture, setProfilePicture] = useState<string | null>(null);
+
+  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+
+    if (file) {
+      try {
+        const storageRef = firebase.storage().ref();
+        const fileRef = storageRef.child(file.name);
+        await fileRef.put(file);
+
+        const downloadURL = await fileRef.getDownloadURL();
+        setProfilePicture(downloadURL);
+      } catch (error) {
+        console.error('Error uploading profile picture:', error);
+      }
+    }
+  };
+
+  return (
+    <div className="upload-area">
+      <input type="file" onChange={handleFileChange} accept="image/*" />
+      <label>
+        <span>Click to upload profile picture</span>
+      </label>
+      {profilePicture && (
+        <img
+          src={profilePicture}
+          alt="Profile Picture"
+          style={{
+            width: 'min(min(50vw,50vh),250px)',
+            height: 'min(min(50vw,50vh),250px)',
+            borderRadius: '50%',
+            backgroundColor: '#fff',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default ProfilePictureUpload;
