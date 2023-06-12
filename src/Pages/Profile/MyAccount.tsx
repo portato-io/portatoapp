7cdcbd8e00391bebb55437b85408358d18a2e3fc
mAkeddar <mehdi.akeddar@gmail.com>
2023-06-12 22:17:38 +0200

    first profile picture upload

--- src/Pages/Profile/MyAccount.tsx
+++ src/Pages/Profile/MyAccount.tsx
@@ -1,30 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import { initializeApp } from 'firebase/app';
-import { auth } from '../../firebaseConfig';
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
+import { auth, storageRef } from '../../firebaseConfig';
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import { getAuth } from 'firebase/auth';
+import { updateProfile, sendEmailVerification } from 'firebase/auth';
 
 import ProfilePageLayout from '../Layouts/ProfilePagesLayout';
 import { Form, ImageUploader } from 'antd-mobile';
-import UploadImage from '../../Components/UploadImage';
-import { Input } from 'antd';
+// import UploadImage from '../../Components/UploadImage';
+import { Upload, Input } from 'antd';
 
 const MyAccount: React.FC = () => {
   const [displayName, setDisplayName] = useState<string | null>(null);
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchDisplayName = async () => {
-      const user = auth.currentUser;
+  const user = auth.currentUser;
 
-      if (user) {
-        const idTokenResult = await user.getIdTokenResult();
-        setDisplayName(idTokenResult.claims.name);
-      }
-    };
-
-    fetchDisplayName();
-  }, []);
+  const uploadProfilePicture = async (file: File) => {
+    console.log(file);
+    if (user) {
+      updateProfile(user, {
+        photoURL: 'file',
+      }).then(function () {
+        console.log('Image uploaded');
+      });
+    }
+  };
+  const onChange = () => {
+    console.log('rien');
+  };
+  const beforeUpload = () => {
+    console.log('rien');
+  };
+  if (user) {
+    updateProfile(user, {
+      displayName: 'customer ',
+      photoURL: 'https://firebasestorage.googleapis.com/v0/b/',
+    }).then(function () {
+      setDisplayName(user.displayName);
+      console.log('TEST', user.displayName);
+      console.log('TESTImage', user.photoURL);
+    });
+  }
+  const [loading, setLoading] = useState(false);
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
   console.log(displayName);
   return (
     <ProfilePageLayout>
@@ -43,7 +68,13 @@ const MyAccount: React.FC = () => {
           <Form.Item
             label={<label className="item-form-label">Profile Picture</label>}
           >
-            <UploadImage />
+            <Upload
+              listType="picture-card"
+              beforeUpload={(file: File) => {
+                uploadProfilePicture(file);
+                return false; // Prevent default upload behavior
+              }}
+            />
           </Form.Item>
           <Form.Item label={<label className="item-form-label">Name</label>}>
             <Input />
@@ -52,6 +83,25 @@ const MyAccount: React.FC = () => {
             label={<label className="item-form-label">Mail address</label>}
           >
             <Input />
+            <Upload
+              name="avatar"
+              listType="picture-card"
+              className="avatar-uploader"
+              showUploadList={false}
+              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+              onChange={onChange}
+              beforeUpload={(file: File) => {
+                uploadProfilePicture(file);
+                return false; // Prevent default upload behavior
+              }}
+            >
+              {' '}
+              {imageUrl ? (
+                <img src={imageUrl} alt="avatar" style={{ width: '100%' }} />
+              ) : (
+                uploadButton
+              )}
+            </Upload>
           </Form.Item>
           <Form.Item
             label={<label className="item-form-label">Living address</label>}
