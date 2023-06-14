f9c6ef52393a02db21260f8d5cf1a82afeaa366c
Mischa Kolbe <24309085+mischakolbe@users.noreply.github.com>
2023-06-15 01:26:29 +0200

    Switched Home page from in-line-style to CSS.

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import { Typography } from 'antd';
 import PageLayout from './Layouts/PageLayoutTest';
+require('../CSS/PortatoStyleSheet.css');
 
 const { Title } = Typography;
 
 const Home: React.FC = () => {
   return (
     <PageLayout>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-        }}
-      >
+      <div className="temp-home-page-css">
         <Title level={2} style={{ textAlign: 'center' }}>
           This is our Home page
         </Title>
