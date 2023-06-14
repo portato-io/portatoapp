e25ab82b56e499e04f2cf599e930c081f4319f22
Mischa Kolbe <24309085+mischakolbe@users.noreply.github.com>
2023-06-15 01:32:11 +0200

    Added defaultActiveKey test expression, updated to new CSS name and added TODOs.

--- src/Pages/Driver/Deliver.tsx
+++ src/Pages/Driver/Deliver.tsx
@@ -4,17 +4,21 @@ import { AutoCenter, Tabs } from 'antd-mobile';
 import RoutesContent from '../../Components/PageComponents/RoutesContent';
 import DealsContent from '../../Components/PageComponents/DealsContents';
 
+// TODO Mischa: Determine whether user has a route or not. If not; show routes page!
+const HAS_ROUTES = false;
+
 const Deliver: React.FC = () => {
   return (
     <PageLayout>
-      <Tabs style={{}}>
+      {/* TODO Mischa: Move the tabs to the bottom of the screen - at least on mobile! */}
+      <Tabs defaultActiveKey={HAS_ROUTES ? 'deals' : 'routes'}>
         <Tabs.Tab title="Deals" key="deals">
-          <AutoCenter className="contentContainer">
+          <AutoCenter className="deliver-overview-content-container">
             <DealsContent />
           </AutoCenter>
         </Tabs.Tab>
         <Tabs.Tab title="Routes" key="routes">
-          <AutoCenter className="contentContainer">
+          <AutoCenter className="deliver-overview-content-container">
             <RoutesContent />
           </AutoCenter>
         </Tabs.Tab>
