09ed9b1561ba032eee88150733d664b6a5c068f8
Mischa Kolbe <mischakolbe@gmail.com>
2023-06-17 23:27:19 +0200

    Using translation for route registering button and removed unnecessary "Current routes" section for now. Use the sendRequest page for reference, when implementing this route page.

--- src/Components/PageComponents/RoutesContent.tsx
+++ src/Components/PageComponents/RoutesContent.tsx
@@ -1,32 +1,23 @@
+import React, { useContext } from 'react';
 import { AutoCenter, Button } from 'antd-mobile';
 import { useNavigate } from 'react-router-dom';
 import { ButtonToCreateNewReqRoutes } from '../Buttons/ButtonToCreateNewReqRoutes';
+import { TranslationContext } from '../../Contexts/TranslationContext';
 
 const NEXT_SCREEN = '/deliver/enterRoute';
 const BUTTON_TEXT = 'Create new route';
 
 function RoutesContent() {
+  const { t } = useContext(TranslationContext);
   return (
     <div>
       <AutoCenter>
         <div style={{ height: '50px' }}></div>
         <ButtonToCreateNewReqRoutes
           nextScreen={NEXT_SCREEN}
-          text={BUTTON_TEXT}
+          text={t('driveOverview.newTitle')}
         />
       </AutoCenter>
-
-      <h1
-        style={{
-          marginTop: '10vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        Your Current Routes
-      </h1>
 
       {/* TODO: Fetch routes and display them here, similar to send requests! */}
     </div>
