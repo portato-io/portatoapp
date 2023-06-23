42743237d861b9370049b579a8b6c5dfa5e1ef83
hugo2410 <hugobirch@hotmail.fr>
2023-06-23 15:03:57 +0200

    adds loading to authprovider

--- src/Components/AuthProvider.tsx
+++ src/Components/AuthProvider.tsx
@@ -1,40 +1,59 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
-import { onAuthStateChanged, getIdTokenResult } from 'firebase/auth';
+import { onAuthStateChanged, getIdTokenResult, User } from 'firebase/auth';
 import { auth } from '../firebaseConfig';
 
-const AuthContext = createContext<{
+interface AuthState {
   uid: string | undefined;
   isAdmin: boolean | undefined;
-}>({
+  user: User | null;
+  loading: boolean;
+}
+
+const initialState: AuthState = {
   uid: undefined,
   isAdmin: undefined,
-});
+  user: null,
+  loading: true,
+};
+
+const AuthContext = createContext<AuthState>(initialState);
 
 interface AuthProviderProps {
   children: React.ReactNode;
 }
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-  const [uid, setUid] = useState<string | undefined>(undefined);
-  const [isAdmin, setIsAdmin] = useState<boolean | undefined>(undefined);
+  const [uid, setUid] = useState<string | undefined>(initialState.uid);
+  const [isAdmin, setIsAdmin] = useState<boolean | undefined>(
+    initialState.isAdmin
+  );
+  const [user, setUser] = useState<User | null>(initialState.user);
+  const [loading, setLoading] = useState<boolean>(initialState.loading);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
         setUid(user.uid);
+        setUser(user);
         const token = await getIdTokenResult(user);
         setIsAdmin(!!token.claims.admin);
       } else {
         setUid(undefined);
         setIsAdmin(undefined);
+        setUser(null);
       }
+      setLoading(false);
     });
 
     return () => unsubscribe();
   }, []);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <AuthContext.Provider value={{ uid, isAdmin }}>
+    <AuthContext.Provider value={{ uid, isAdmin, user, loading }}>
       {children}
     </AuthContext.Provider>
   );
@@ -45,4 +64,9 @@ export const useAuth = () => useContext(AuthContext);
 export const checkAdmin = () => {
   const { isAdmin } = useAuth();
   return isAdmin;
+};
+
+export const getUser = () => {
+  const { user } = useAuth();
+  return user;
 };
