0863e84f29a1d1f9cc4bd01405482695d757cebf
emgerold <buero@emgerold.de>
2023-06-24 10:32:47 +0300

    setup inital css, added fonts and icons, cleaned html

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+import './CSS/Fonts.css';
+import './CSS/Icons.css';
+import './CSS/Forms.css';
 import './CSS/Core.css';
+
 import React, { Suspense, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SideNavigator from './Components/SideBarNav';
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { Row, Col, Typography, Card, Space, Avatar } from 'antd';
 import PageLayout from './Layouts/PageLayoutTest';
-import backgroundImg from '../Assets/Images/landing_page_image2.png';
+import headerImage from '../Assets/Images/portato-landing-4.png';
 import portatoLogo from '../Assets/Images/logo_blue.png';
 import howItWorksImg from '../Assets/Images/how_it_works.gif';
 import { TranslationContext } from '../Contexts/TranslationContext';
@@ -39,98 +39,91 @@ const Home: React.FC = () => {
   return (
     <PageLayout>
       {/* Landing screen */}
-      <section className="landing-section">
-        <div className="landing-main-container">
-          <div className="landing-main-labels">
-            <div className="landing-portato-logo">
-              <img
-                src={portatoLogo}
-                alt="Portato Icon"
-                className="portato-icon"
-              ></img>
-              <h1 className="font-comfortaa portato-wordmark">portato</h1>
-            </div>
+      <section className="section section-bleed">
+        <img
+          src={headerImage}
+          alt="portato header image"
+          className="image header-image"
+        />
+      </section>
 
-            <p className="font-comfortaa portato-tagline">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
-          </div>
+      <section className="section">
+        <div className="text-section">
+          <h1 className="icon icon-logo icon-big logo-slogan">portato</h1>
+          <h3>A easy, cheap and eco-friendly transport service for things.</h3>
         </div>
       </section>
 
-      <div className="home-main-container">
-        {/* Call to action */}
-        <section className="cta-section section-container">
-          <div className="cta-button-layout">
-            <button
-              className="cta-button font-comfortaa"
-              onClick={() =>
-                (window.location.href = '/createSendRequest/enterObjInfo')
-              }
-            >
-              Send something
-            </button>
-            <button
-              className="cta-button font-comfortaa"
-              onClick={() => (window.location.href = '/deliver/enterRoute')}
-            >
-              Deliver something
-            </button>
-          </div>
-        </section>
+      <section className="section">
+        <div className="spacer-regular"></div>
+        <div className="mod-display-flex">
+          <a
+            className="button button-solid button-shadow"
+            href="/createSendRequest/enterObjInfo"
+          >
+            Send
+          </a>
+          <a
+            className="button button-solid button-shadow"
+            href="/deliver/enterRoute"
+          >
+            Deliver
+          </a>
+        </div>
+      </section>
 
-        {/* How it works */}
-        {/* <lottie-player src="../Assets/Lotties/test-animation.json" background="transparent"  speed="1" loop controls autoplay></lottie-player> */}
-        {/* <lottie-player src="https://assets9.lottiefiles.com/datafiles/MUp3wlMDGtoK5FK/data.json"  background="transparent"  loop controls autoplay></lottie-player> */}
-        {/* <lottie-player src="https://assets9.lottiefiles.com/datafiles/MUp3wlMDGtoK5FK/data.json" background="transparent" loop autoplay></lottie-player> */}
-        {/* <lottie-player src="../Assets/Lotties/test-animation.json" background="transparent" loop autoplay></lottie-player> */}
-        {/* <lottie-player
+      <section className="section">
+        <Card
+          size="small"
+          title={t('about.title')}
+          style={{ width: '90vw', margin: 'auto' }}
+        >
+          <h3>{t('about.visionMissionTitle')}</h3>
+          <p>
+            {t('about.visionMissionText')
+              .split('\n')
+              .map((line: string, i: number) => (
+                <span key={i}>
+                  {line}
+                  <br />
+                </span>
+              ))}
+          </p>
+          <h3>{t('about.whoWeAreTitle')}</h3>
+          <p>
+            {t('about.whoWeAreText')
+              .split('\n')
+              .map((line: string, i: number) => (
+                <span key={i}>
+                  {line}
+                  <br />
+                </span>
+              ))}
+          </p>
+
+          {/* Team members */}
+
+          <Row gutter={[16, 16]} justify="center">
+            {teamMemberNames.map((name) => (
+              <TeamMember memberName={name} key={name} />
+            ))}
+          </Row>
+        </Card>
+      </section>
+
+      {/* How it works */}
+      {/* <lottie-player src="../Assets/Lotties/test-animation.json" background="transparent"  speed="1" loop controls autoplay></lottie-player> */}
+      {/* <lottie-player src="https://assets9.lottiefiles.com/datafiles/MUp3wlMDGtoK5FK/data.json"  background="transparent"  loop controls autoplay></lottie-player> */}
+      {/* <lottie-player src="https://assets9.lottiefiles.com/datafiles/MUp3wlMDGtoK5FK/data.json" background="transparent" loop autoplay></lottie-player> */}
+      {/* <lottie-player src="../Assets/Lotties/test-animation.json" background="transparent" loop autoplay></lottie-player> */}
+      {/* <lottie-player
           src="https://lottie.host/dd830d03-3456-47ab-a89e-c64105f8ca2e/nGZ4DRarLa.json"
           background="transparent"
           loop
           autoplay
         ></lottie-player> */}
 
-        {/* About us */}
-        <section>
-          <Card
-            size="small"
-            title={t('about.title')}
-            style={{ width: '90vw', margin: 'auto' }}
-          >
-            <h3>{t('about.visionMissionTitle')}</h3>
-            <p>
-              {t('about.visionMissionText')
-                .split('\n')
-                .map((line: string, i: number) => (
-                  <span key={i}>
-                    {line}
-                    <br />
-                  </span>
-                ))}
-            </p>
-            <h3>{t('about.whoWeAreTitle')}</h3>
-            <p>
-              {t('about.whoWeAreText')
-                .split('\n')
-                .map((line: string, i: number) => (
-                  <span key={i}>
-                    {line}
-                    <br />
-                  </span>
-                ))}
-            </p>
-
-            {/* Team members */}
-
-            <Row gutter={[16, 16]} justify="center">
-              {teamMemberNames.map((name) => (
-                <TeamMember memberName={name} key={name} />
-              ))}
-            </Row>
-          </Card>
-        </section>
-      </div>
+      {/* About us */}
     </PageLayout>
   );
 };
