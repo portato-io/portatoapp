199248d6031ce30b8f3df8bd6034c50b5cf8aef3
mAkeddar <mehdi.akeddar@gmail.com>
2023-09-23 11:34:05 +0200

    form for additional info/ not linked to firebase

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,46 +1,82 @@
 import React from 'react';
-import PageLayout from './Layouts/PageLayout';
+import { Form, Input, Button, DatePicker } from 'antd';
+import PageLayout from './Layouts/PageLayoutTest';
+import { useTranslation } from 'react-i18next';
 
-function About() {
+const About = () => {
+  const { t } = useTranslation<string>();
+  // Function to handle form submission
+  const onFinish = (values: any) => {
+    console.log('Form values:', values);
+    // You can perform actions like sending the data to a server here
+  };
+
   return (
     <PageLayout>
-      <div className="main-body-section">
-        <div className="container">
-          <div className="block-title">
-            <h2>About Us</h2>
-          </div>
-          <div className="about-content">
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque
-              cupiditate omnis eius totam aperiam? Illo eius repudiandae nostrum
-              iste dignissimos ad molestias facere atque, itaque placeat dolores
-              dolorum doloremque? Sint.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
-              impedit sapiente tempore veritatis neque minima laudantium eius
-              magni aut aliquid esse sequi, hic delectus nihil temporibus quas
-              nobis porro exercitationem? Repellendus ipsa animi enim id sint
-              ullam beatae eius repudiandae consequuntur nisi, eum quia, ad iure
-              similique, tenetur cum voluptatibus sit natus rem eos iusto quam
-              eveniet et quidem. Est distinctio minima sequi quas provident hic
-              nulla maiores quam quaerat. Corrupti unde illum suscipit autem
-              iure maxime dolorem magni ipsam! Ad officia veritatis aperiam
-              nemo. Voluptas at nostrum molestias incidunt necessitatibus,
-              sapiente numquam facere explicabo quos, eum cumque repellat quo.
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet
-              sunt ullam nesciunt beatae deleniti similique aliquam excepturi,
-              minus nobis minima ex atque, quos veniam architecto doloribus
-              maxime sapiente. Vel voluptatem pariatur delectus dolor quidem
-              eveniet repellat facere tempora nisi. Ut!
-            </p>
-          </div>
-        </div>
-      </div>
+      <section className="section section-form mod-nomargin-top">
+        <Form
+          name="myForm"
+          onFinish={onFinish}
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 14 }}
+          layout="horizontal"
+          style={{ marginTop: '5vh' }}
+        >
+          <h2>{t('additionalInfo.title')}</h2>
+          <Form.Item
+            label={t('additionalInfo.address')}
+            name="address"
+            className="input-wrapper"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter your address!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          {/* Phone Number Field */}
+          <Form.Item
+            className="input-wrapper"
+            label={t('additionalInfo.phoneNum')}
+            name="phoneNumber"
+            rules={[
+              {
+                required: true,
+                message: 'Please enter your phone number!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          {/* Birthday Field */}
+          <Form.Item
+            className="input-wrapper"
+            label={t('additionalInfo.birthday')}
+            name="birthday"
+            rules={[
+              {
+                required: true,
+                message: 'Please select your birthday!',
+              },
+            ]}
+          >
+            <DatePicker style={{ width: '100%' }} />
+          </Form.Item>
+
+          {/* Submit Button */}
+          <Form.Item wrapperCol={{ offset: 6, span: 14 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </section>
     </PageLayout>
   );
-}
+};
 
 export default About;
