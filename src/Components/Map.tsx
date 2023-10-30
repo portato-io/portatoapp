dcf53c1cdf0defc555db73d62270ee7240125f37
mAkeddar <mehdi.akeddar@gmail.com>
2023-10-30 15:57:56 +0100

    test and correct data structure for map markers

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -8,15 +8,63 @@ export declare type MapOptions = {
 };
 
 require('../CSS/Map.css');
+import 'mapbox-gl/dist/mapbox-gl.css'; // Import Mapbox GL JS CSS
+
 mapboxgl.accessToken =
   'pk.eyJ1IjoicG9ydGF0byIsImEiOiJjbG05OXgweWMwZTc1M2d0Zm5oNWJsMWJoIn0.jv7429nwZERiVl1f6xY3QA';
 
+interface Coordinates {
+  type: 'Point';
+  coordinates: [number, number];
+}
 function Map() {
   const mapContainer = useRef<HTMLDivElement | null>(null);
   const map = useRef<mapboxgl.Map | null>(null); // Define the custom type for 'map'
   // const [lng, setLng] = useState(0);
   // const [lat, setLat] = useState(0);
   // const [zoom, setZoom] = useState(9);
+  // const test = {
+  //   type: 'FeatureCollection',
+  //   features: [
+  //     {
+  //       type: 'Feature',
+  //       geometry: {
+  //         type: 'Point',
+  //         coordinates: [46.5196535, 6.6322734],
+  //       },
+  //       properties: {
+  //         title: 'Mapbox',
+  //         description: 'CUSTOM PIN',
+  //       },
+  //     },
+  //   ],
+  // };
+  const features: Array<{
+    type: 'Feature';
+    geometry: Coordinates;
+  }> = [
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [6.6322734, 46.5196535],
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [6.6522778, 46.5196535],
+      },
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [6.7022785, 46.5196535],
+      },
+    },
+  ];
 
   const displayMarkers = function (options: MapOptions) {
     console.log(options);
@@ -38,6 +86,24 @@ function Map() {
             center: [longitude, latitude],
             zoom: 9,
           });
+          if (map.current) {
+            for (const feature of features) {
+              console.log(feature);
+              // create a HTML element for each feature
+              const el = document.createElement('div');
+              el.className = 'marker';
+              // make a marker for each feature and add to the map
+              new mapboxgl.Marker()
+                .setLngLat(feature.geometry.coordinates)
+                .setPopup(
+                  new mapboxgl.Popup({ offset: 25 }) // add popups
+                    .setHTML(`<h3>${'blabl'}</h3><p>${'DESCRIPTION'}</p>`)
+                )
+                .addTo(map.current!);
+
+              map.current.setZoom(12); // Adjust the zoom level as needed
+            }
+          }
         },
 
         (error) => {
@@ -50,6 +116,7 @@ function Map() {
 
     // if (map.current) return; // Initialize map only once
   }, []);
+
   return <div ref={mapContainer} className="map-wrapper" />;
 }
 export default Map;
