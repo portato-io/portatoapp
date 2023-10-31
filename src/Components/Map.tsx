dda6db8227a0699a39444a4cf567f2711c880063
mAkeddar <mehdi.akeddar@gmail.com>
2023-10-31 22:12:24 +0100

    working fetch and structure for Map and portato map interaction, next step : fetch all the data -> filter delivery adress and departure -> display

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -17,58 +17,10 @@ interface Coordinates {
   type: 'Point';
   coordinates: [number, number];
 }
-function Map() {
+
+function Map({ geoDatas }: any) {
   const mapContainer = useRef<HTMLDivElement | null>(null);
   const map = useRef<mapboxgl.Map | null>(null); // Define the custom type for 'map'
-  // const [lng, setLng] = useState(0);
-  // const [lat, setLat] = useState(0);
-  // const [zoom, setZoom] = useState(9);
-  // const test = {
-  //   type: 'FeatureCollection',
-  //   features: [
-  //     {
-  //       type: 'Feature',
-  //       geometry: {
-  //         type: 'Point',
-  //         coordinates: [46.5196535, 6.6322734],
-  //       },
-  //       properties: {
-  //         title: 'Mapbox',
-  //         description: 'CUSTOM PIN',
-  //       },
-  //     },
-  //   ],
-  // };
-  const features: Array<{
-    type: 'Feature';
-    geometry: Coordinates;
-    class: string;
-  }> = [
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [6.6322734, 46.5196535],
-      },
-      class: 'box-marker',
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [6.6522778, 46.5196535],
-      },
-      class: 'car-marker',
-    },
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'Point',
-        coordinates: [6.7022785, 46.5196535],
-      },
-      class: 'box-marker',
-    },
-  ];
 
   const displayMarkers = function (options: MapOptions) {
     console.log(options);
@@ -90,15 +42,15 @@ function Map() {
             zoom: 9,
           });
           if (map.current) {
-            for (const feature of features) {
-              console.log(feature);
-              // create a HTML element for each feature
+            for (const geoData of geoDatas) {
+              console.log(geoData);
+              // create a HTML element for each geoData
               const el = document.createElement('div');
-              // make a marker for each feature and add to the map
-              el.className = feature.class;
+              // make a marker for each geoData and add to the map
+              el.className = geoData.class;
 
               new mapboxgl.Marker(el)
-                .setLngLat(feature.geometry.coordinates)
+                .setLngLat(geoData.geometry.coordinates)
                 .setPopup(
                   new mapboxgl.Popup({ offset: 25 }) // add popups
                     .setHTML(`<h3>${'blabl'}</h3><p>${'DESCRIPTION'}</p>`)
--- src/Pages/PortatoMap.tsx
+++ src/Pages/PortatoMap.tsx
@@ -1,49 +1,103 @@
 import React, { useEffect, useRef, useState } from 'react';
 import PageLayout from './Layouts/PageLayoutTest';
-import { fetchGeoData } from '../linksStoreToFirebase';
+import { fetchDataOnce, fetchGeoData } from '../linksStoreToFirebase';
 import { useAuth } from '../Components/AuthProvider';
 import { useTranslation } from 'react-i18next';
 import { logEvent } from 'firebase/analytics';
 import { analytics } from '../firebaseConfig';
 import Map from '../Components/Map';
 import { IRequestInfo, MapMarker } from '../type';
+import AllRequests from './Admin/AllRequests';
+import {
+  setKey,
+  setDefaults,
+  setLanguage,
+  setRegion,
+  fromAddress,
+  fromLatLng,
+  fromPlaceId,
+  setLocationType,
+  geocode,
+  RequestType,
+} from 'react-geocode';
+import { features } from 'process';
+
+setKey('AIzaSyAE3IxTyHt4PIQ1t4PB1rAr0ZMTIpJd0DE');
+
+interface Coordinates {
+  type: 'Point';
+  coordinates: [number, number];
+}
 
 const PortatoMap: React.FC = () => {
   const { user } = useAuth();
-  const [geoData, setGeoData] = useState<MapMarker[]>([]);
+  const [geoData, setGeoData] = useState<string>();
 
-  useEffect(() => {
+  const test = [
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [6.6322734, 46.5196535],
+      },
+      class: 'box-marker',
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [6.6522778, 46.5196535],
+      },
+      class: 'car-marker',
+    },
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [6.7022785, 46.5196535],
+      },
+      class: 'box-marker',
+    },
+  ];
+
+  const fetchData = async () => {
     if (!user) {
       console.log('user is undefined');
       return;
     }
+    try {
+      const fetchData = await fetchDataOnce(user.uid, 'requests');
+      // console.log('AAAH', Object.values(fetchData));
+      let geoDataArray: IRequestInfo[] = [];
 
-    const fetchData = async () => {
-      try {
-        const fetchData = await fetchGeoData(user.uid, 'requests');
-        let geoDataArray: MapMarker[] = [];
-
-        // Check if fetchData is not null or undefined before returning it
-        if (fetchData) {
-          geoDataArray = Object.values(fetchData) as MapMarker[];
-
-          // Check if fetchData is an object before returning it
-          if (fetchData && typeof fetchData === 'object') {
-            geoDataArray = Object.values(fetchData) as MapMarker[];
-            setGeoData(geoDataArray);
-          } else {
-            console.log('Data is not an object:', fetchData);
-          }
-
-          setGeoData(geoData);
+      // Check if fetchData is not null or undefined before returning it
+      if (fetchData) {
+        // geoDataArray = Object.values(fetchData) as IRequestInfo[];
+        // console.log('MAAAAAAM', geoDataArray);
+        // Check if fetchData is an object before returning it
+        if (fetchData && typeof fetchData === 'object') {
+          geoDataArray = Object.values(fetchData) as IRequestInfo[];
+          console.log('PAAAAAAAAAAP', geoDataArray[0].delivery_adress);
+          setGeoData(geoDataArray[0].delivery_adress);
+        } else {
+          console.log('Data is not an object:', fetchData);
         }
-      } catch (error) {
-        console.log('Error fetching data: ', error);
       }
-    };
-
+    } catch (error) {
+      console.log('Error fetching data: ', error);
+    }
+  };
+  useEffect(() => {
     fetchData();
-  });
+    console.log(geoData);
+    if (geoData)
+      fromAddress(geoData)
+        .then(({ results }) => {
+          const { lat, lng } = results[0].geometry.location;
+          console.log('MAMAN', lat, lng);
+        })
+        .catch(console.error);
+  }, [user?.uid]);
 
   return (
     <PageLayout>
@@ -51,7 +105,7 @@ const PortatoMap: React.FC = () => {
         <h1>Map</h1>
       </section>
       <section className="section section-bleed portato-map">
-        <Map />
+        <Map geoDatas={test} />
       </section>
     </PageLayout>
   );
