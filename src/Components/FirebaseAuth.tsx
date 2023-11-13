d65a2736ea7d156102a9878c5c1a5a8c8bb2842d
hugo2410 <hugobirch@hotmail.fr>
2023-11-13 11:48:22 +0100

    flow works

--- src/Components/FirebaseAuth.tsx
+++ src/Components/FirebaseAuth.tsx
@@ -1,102 +1,59 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
 import 'firebaseui/dist/firebaseui.css';
 import {
   EmailAuthProvider,
   RecaptchaVerifier,
-  createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signInWithPhoneNumber,
 } from 'firebase/auth';
-import { uiInstance, uiConfig } from './firebaseUIInstance';
 import { auth } from '../firebaseConfig';
 import { Button, message } from 'antd';
 import { ConfirmationResult } from 'firebase/auth'; // Import the type
 import { linkWithCredential } from 'firebase/auth';
 
-const configureCaptcha = () => {
-  const test = document.getElementById('grecaptcha-button');
-  console.log(test);
-  let recaptchaVerifier;
-  if (test) {
-    console.log('create captcha');
-    recaptchaVerifier = new RecaptchaVerifier(
-      test,
-      {
-        size: 'invisible',
-        callback: (response: any) => {
-          console.log('hi', response);
-          console.log('Recaptca varified');
-        },
-      },
-      auth
-    );
-    console.log(recaptchaVerifier);
-  }
-};
-
 const FirebaseAuth: React.FC = () => {
-  const uiRef = useRef<HTMLDivElement | null>(null);
-
-  // useEffect(() => {
-  //   configureCaptcha();
-  //   // Update the signInSuccessUrl and callbacks in uiConfig
-  //   uiConfig.signInOptions = [EmailAuthProvider.PROVIDER_ID];
-  //   uiConfig.callbacks = {
-  //     signInSuccessWithAuthResult: () => {
-  //       window.location.replace(window.location.pathname);
-  //       return false;
-  //     },
-  //   };
-
-  //   // Start the FirebaseUI widget
-  //   uiInstance.start('#firebaseui-auth-container', uiConfig);
-
-  //   // Cleanup on unmount
-  //   return () => {
-  //     uiInstance.reset();
-  //   };
-  // }, []);
   const [mynumber, setnumber] = useState('');
   const [otp, setotp] = useState('');
   const [show, setshow] = useState(false);
-  const [final, setfinal] = useState('');
 
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [confirmationResult, setConfirmationResult] =
     useState<ConfirmationResult | null>(null);
   const [isSignUpSuccessful, setIsSignUpSuccessful] = useState(false);
+  const recaptchaVerifierRef = useRef<RecaptchaVerifier | null>(null);
 
-  const createAccount = async () => {
-    const verify = new RecaptchaVerifier('recaptcha-container', {}, auth);
-    try {
-      const result = await signInWithPhoneNumber(auth, mynumber, verify);
-      setConfirmationResult(result); // Save the confirmation result
-      alert('Code sent');
-      setshow(true);
-    } catch (error) {
-      console.error(error);
+  const [isCaptchaVerified, setIsCaptchaVerified] = useState(false);
+  const [step, setStep] = useState('initial'); // 'initial', 'captchaVerified', 'smsSent', 'otpEntered', 'signedUp'
+
+  // Initialize the reCAPTCHA verifier in useEffect when the sign-up process begins
+
+  useEffect(() => {
+    if (step === 'signUp' && !recaptchaVerifierRef.current) {
+      recaptchaVerifierRef.current = new RecaptchaVerifier(
+        'recaptcha-container',
+        {
+          size: 'normal',
+          callback: (response: string) => {
+            setIsCaptchaVerified(true);
+            message.success('Captcha verification successful');
+          },
+        },
+        auth
+      );
+      recaptchaVerifierRef.current.render();
     }
-  };
 
-  const ValidateOtp = async () => {
-    if (otp === null || otp === '') return;
-    try {
-      const result = await confirmationResult?.confirm(otp);
-      const email = emailRef.current?.value || ''; // Fallback to empty string if undefined
-      const password = passwordRef.current?.value || ''; // Fallback to empty string if undefined
-
-      const credential = EmailAuthProvider.credential(email, password);
-      if (result?.user) {
-        await linkWithCredential(result.user, credential);
-        // Handle successful link here
-        setIsSignUpSuccessful(true);
+    // Cleanup function to reset reCAPTCHA
+    return () => {
+      if (window.recaptchaWidgetId !== undefined) {
+        recaptchaVerifierRef.current?.clear();
+        recaptchaVerifierRef.current = null;
       }
-    } catch (error) {
-      console.error('Error during OTP validation or account linking:', error);
-      // Handle errors here
-    }
-  };
+    };
+  }, [step]);
 
   const signIn = async () => {
     try {
@@ -114,40 +71,66 @@ const FirebaseAuth: React.FC = () => {
     await auth.signOut();
   };
 
-  const [showSignUp, setShowSignUp] = useState(false);
-  const [showSignIn, setShowSignIn] = useState(false);
-
-  const handleShowSignUp = () => {
-    setShowSignUp(true);
-    setShowSignIn(false);
+  const onSendSMS = async () => {
+    const currentEmail = emailRef.current?.value || '';
+    const currentPassword = passwordRef.current?.value || '';
+    setEmail(currentEmail);
+    setPassword(currentPassword);
+    console.log('Trying to send SMS');
+    if (!isCaptchaVerified || !recaptchaVerifierRef.current) {
+      message.error('Please complete the reCAPTCHA challenge.');
+      return;
+    }
+    try {
+      const result = await signInWithPhoneNumber(
+        auth,
+        mynumber,
+        recaptchaVerifierRef.current
+      );
+      setConfirmationResult(result);
+      setStep('smsSent');
+      message.success('SMS sent successfully. Please enter the OTP.');
+    } catch (error) {
+      console.error('SMS sending error:', error);
+      message.error('Failed to send SMS');
+    }
   };
 
-  const handleShowSignIn = () => {
-    setShowSignIn(true);
-    setShowSignUp(false);
-  };
-
-  const handleBack = () => {
-    setShowSignUp(false);
-    setShowSignIn(false);
+  const onVerifyOtp = async () => {
+    if (otp === null || otp === '') return;
+    try {
+      const result = await confirmationResult?.confirm(otp);
+      if (result?.user) {
+        // Link email and password if provided
+        if (email && password) {
+          const credential = EmailAuthProvider.credential(email, password);
+          await linkWithCredential(result.user, credential);
+        } else {
+          message.success(
+            'Failed to sign up due to missing email adress or password !'
+          );
+        }
+        setStep('signedUp');
+        message.success('Sign up successful!');
+      }
+    } catch (error) {
+      console.error('OTP verification error:', error);
+      message.error('Failed to verify OTP');
+    }
   };
 
   return (
     <div style={{ marginTop: '200px' }}>
       <center>
-        {!showSignUp && !showSignIn && (
+        {step === 'initial' && (
           <>
-            <button onClick={handleShowSignUp}>Sign Up</button>
-            <button onClick={handleShowSignIn}>Sign In</button>
+            <button onClick={() => setStep('signUp')}>Sign Up</button>
+            <button onClick={() => setStep('signIn')}>Sign In</button>
           </>
         )}
 
-        {(showSignUp || showSignIn) && (
-          <button onClick={handleBack}>Back</button>
-        )}
-
-        {showSignUp && (
-          <div>
+        {step === 'signUp' && (
+          <>
             <input ref={emailRef} type="email" placeholder="Email" />
             <input ref={passwordRef} type="password" placeholder="Password" />
             <input
@@ -155,41 +138,28 @@ const FirebaseAuth: React.FC = () => {
               onChange={(e) => setnumber(e.target.value)}
               placeholder="Phone Number"
             />
-            <br />
-            <br />
             <div id="recaptcha-container"></div>
-            <button onClick={createAccount}>Send SMS</button>
-          </div>
+            {isCaptchaVerified && (
+              <Button type="primary" onClick={onSendSMS}>
+                Send SMS
+              </Button>
+            )}
+          </>
         )}
 
-        {showSignIn && (
-          <div>
-            <input ref={emailRef} type="email" placeholder="Email" />
-            <input ref={passwordRef} type="password" placeholder="Password" />
-            <br />
-            <br />
-            <button onClick={signIn}>Sign in</button>
-          </div>
+        {step === 'smsSent' && (
+          <>
+            <input
+              type="text"
+              placeholder="Enter your OTP"
+              onChange={(e) => setotp(e.target.value)}
+            />
+            <Button onClick={onVerifyOtp}>Verify</Button>
+            <Button onClick={onSendSMS}>Resend SMS</Button>
+          </>
         )}
 
-        <div
-          style={{
-            display: show ? 'block' : 'none',
-          }}
-        >
-          <input
-            type="text"
-            placeholder={'Enter your OTP'}
-            onChange={(e) => {
-              setotp(e.target.value);
-            }}
-          ></input>
-
-          <br />
-          <br />
-          <button onClick={ValidateOtp}>Verify</button>
-        </div>
-        {isSignUpSuccessful && (
+        {step === 'signedUp' && (
           <div style={{ color: 'green', marginTop: '20px' }}>
             Sign up successful!
           </div>
